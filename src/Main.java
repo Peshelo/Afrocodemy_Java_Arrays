5c03454a1d0359b613f35f490fa9b4ce91ffe723
peshelg <peshelg@afrosoft.co.zw>
2024-01-22 10:45:28 +0200

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        // Each Question has its class in the src folder
+        System.out.println("---------Afrocodemy Java Arrays Exercise---------");
 
 
     }
